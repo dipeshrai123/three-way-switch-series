73efd5e9ce8d64ec318f7b2a760c49c07aa7b371
raidipesh78 <raidipesh78@gmail.com>
2021-02-26 11:00:14 +0545

    3 way switch pattern method 2

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // 3 way switch pattern
 // pattern: 1 2 1 0 1 2 1 0 1 2 1 0
+
+// method 1
 const pattern = [];
 let loop = 1;
 for (let i = 0; i <= 20; i++) {
@@ -17,4 +19,17 @@ for (let i = 0; i <= 20; i++) {
   }
 }
 
-console.log(pattern);
+// method 2
+const pattern2 = [];
+
+for (let i = 0; i <= 20; i++) {
+  const mod4 = i % 4;
+
+  if (mod4 === 0 || mod4 === 2) {
+    pattern2.push(mod4);
+  } else if (mod4 === 1 || mod4 === 3) {
+    pattern2.push(1);
+  }
+}
+
+console.log(pattern2);
